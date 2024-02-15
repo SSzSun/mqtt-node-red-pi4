25e8e51b31110b3a13772b1a87226ba60f4c147b
Jaruphat Khenprom <jaruphat.sun@gmail.com>
2024-02-16 04:56:25 +0700

    Update mqtt-nodered.py

--- mqtt-nodered.py
+++ mqtt-nodered.py
@@ -1,53 +1,48 @@
-
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import time
+
 LED_PIN = 16
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
     client.subscribe("rasp/v_s")
+    if rc == 0:
+        client.publish("pi/status", 1)
+        print("Connected successfully\n==================")
+    else:
+        client.publish("pi/status", 3)
+        print("Connection failed with code", rc)
 
 def on_message(client, userdata, msg):
-
     received_data = msg.payload
     decoded_data = received_data.decode('utf-8')
 
-    # print(msg.topic+" = "+str(msg.payload))
-    # print(msg.topic+" = "+decoded_data)
-    # print(type(msg.payload))
-
-    # if decoded_data == '10' or decoded_data.isdigit(): 
-    print(f'Server sand : {decoded_data}')
-    if decoded_data == '10':
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        print("LED ON")
-    elif decoded_data == '5' :
-        GPIO.output(LED_PIN, GPIO.LOW)
-        print("LED OFF")
+    print(f'Server sand : {decoded_data}\n')
+    if decoded_data == '1':
+        print(f'Buys')
+    elif decoded_data == '5':
+        client.publish("pi/status", 3)
+        client.disconnect()
     else:
+        print(f'Enter Else\n---------')
+        client.publish("status/wait", 2)
+        time.sleep(3)
+        print(f'Befor-loop\n---------')
+        gpio_loop(5)
+        print(f'After-loop\n---------')
+        client.publish("pi/status", 1)
+        print(f'After-publish\n---------')
+
+def gpio_loop(x):
+    for _ in range(x):
         GPIO.output(LED_PIN, GPIO.HIGH)
         time.sleep(0.5)
         GPIO.output(LED_PIN, GPIO.LOW)
         time.sleep(0.5)
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        time.sleep(0.5)
-        GPIO.output(LED_PIN, GPIO.LOW)
-        time.sleep(0.5)
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        time.sleep(0.5)
-        GPIO.output(LED_PIN, GPIO.LOW)
-        time.sleep(0.5)
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        time.sleep(0.5)
-        GPIO.output(LED_PIN, GPIO.LOW)
-        print("Null")
-        client.publish("pi/s_v", "error")
-    print(f'Server staby')
-        
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
